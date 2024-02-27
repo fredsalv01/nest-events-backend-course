22a1f0b2c7beb85f8882b67d997f1ba5ac5eb29b
Freddy Morales <freddy.morales@usil.pe>
2024-02-27 02:45:47 -0500

    entities and dtos

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -9,40 +9,59 @@ import {
   Post,
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
+import { UpdateEventDto } from './update-event.dto';
+import { Event } from './event.entity';
 
 @Controller({
   path: '/events',
 })
 export class EventsController {
+  private events: Event[] = [];
+
   constructor() {}
 
   @Get()
   findAll() {
-    return [
-      { id: 1, name: 'First event' },
-      { id: 2, name: 'Second event' },
-    ];
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return id;
+    const event = this.events.find((item: Event) => item.id === parseInt(id));
+    return event;
   }
 
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    } as Event;
+
+    this.events.push(event);
+    return event;
   }
 
   @Patch(':id')
   @HttpCode(200)
-  update(@Param('id') id: string, @Body() input) {
-    return input;
+  update(@Param('id') id: string, @Body() input: UpdateEventDto) {
+    const eventIndex = this.events.findIndex(
+      (item: Event) => item.id === parseInt(id),
+    );
+
+    this.events[eventIndex] = {
+      ...this.events[eventIndex],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[eventIndex].when,
+    };
+
+    return this.events[eventIndex];
   }
 
   @Delete(':id')
   @HttpCode(204)
   remove(@Param('id') id: string) {
-    return id;
+    this.events = this.events.filter((item: Event) => item.id !== parseInt(id));
   }
 }
--- src/update-event.dto.ts
+++ src/update-event.dto.ts
@@ -2,6 +2,7 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateEventDto } from "./create-event.dto";
 
 export class UpdateEventDto extends PartialType(CreateEventDto)  {
+  // partial type is always necessary in update dtos
   name?: string;
   description?: string;
   when?: string;
