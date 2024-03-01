6ee5b5bd39f87381aa105732d2c135cc962a4b3c
Freddy Morales <freddy.morales@usil.pe>
2024-03-01 12:28:33 -0500

    one to many relation

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,14 +1,18 @@
 import { registerAs } from '@nestjs/config';
 import { Event } from '../events/event.entity';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Attendee } from "../events/attendee.entity";
 
-export default registerAs ('orm.config', (): TypeOrmModuleOptions => ({
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [Event],
-  synchronize: true,
-}));
+export default registerAs(
+  'orm.config',
+  (): TypeOrmModuleOptions => ({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [Event, Attendee],
+    synchronize: true,
+  }),
+);
