de7a4f88994431edc4db405ddf1b0f6f8e562968
Freddy Morales <freddy.morales@usil.pe>
2024-03-04 03:14:23 -0500

    jwt and passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Event } from './events/event.entity';
 import { EventsModule } from './events/events.module';
 import { ConfigModule } from '@nestjs/config';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +19,7 @@ import ormConfigProd from './config/orm.config.prod';
       useFactory: process.env.NODE_ENV !== 'production' ? ormConfig: ormConfigProd
     }),
     EventsModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
